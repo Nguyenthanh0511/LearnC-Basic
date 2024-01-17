5c4e2fb6656817b0485e4adf70e9b1992173d1a9
rynxu2 <dobaolong209@gmail.com>
2024-01-17 22:38:05 +0700

    a

---
+++ Excample/Excample13/XeTai.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Excample13
+{
+    internal class XeTai : PhuongTien
+    {
+        public double trongTai { get; private set; }
+
+        public XeTai() : base() { }
+
+        public XeTai(string hangSanXuat, int namSanXuat, double giaBan, string mauSac, double trongTai) : base(hangSanXuat, namSanXuat, giaBan, mauSac)
+        {
+            this.trongTai = trongTai;
+        }
+
+        public void NhapDangKy()
+        {
+            base.NhapDangKy();
+            Console.Write("Nhap trong tai: ");
+            this.trongTai = double.Parse(Console.ReadLine());
+        }
+    }
+}
